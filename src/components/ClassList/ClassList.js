839f21b4a8718cd9bd878fc02b27a86063a00fb9
IC Realtime MBP <ICREALTIME@ICR-MacBook-Pro.local>
2019-05-21 15:48:35 -0700

    finished afternoon

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
+import axios from "axios";
+import { Link } from 'react-router-dom';
 
 export default class ClassList extends Component {
   constructor() {
     super()
-    
+      this.state = {
+        students: []
+      }
   }
 
+// match is an object with useful info
+// params is a property of match- it is the value of any route parameters
+// class is the route parameter
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`).then(results => {
+    this.setState({
+    students: results.data
+      })
+  })
+}
+
+
+
+
   render() {
+    const students = this.state.students.map((student, i ) => (
+      <Link to={`/student/${student.id}`} key={i}>
+       <h3>{ student.first_name } { student.last_name}</h3>
+      </Link>
+    ))
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
+        { students }
 
       </div>
     )
